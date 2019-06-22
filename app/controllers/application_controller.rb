279cf42ea37059a1b456afa65855b20c84b799ef
MKanko <mkanko66@gmail.com>
2019-06-22 11:07:42 -0700

    completed sinatra-ar-crud-lab

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,4 +33,23 @@ class ApplicationController < Sinatra::Base
     erb :show 
   end 
 
+  get '/articles/:id/edit' do  #edit 
+    @article = Article.find(params[:id])
+    erb :edit 
+  end 
+
+  patch '/articles/:id' do  #update 
+    @article = Article.find(params[:id])
+    @article.update(params[:article]) 
+    redirect to "/articles/#{@article.id}"
+  end 
+
+  delete '/articles/:id' do  #delete
+    Article.destroy(params[:id])
+    redirect to '/articles'
+  end 
+
+
+
+
 end
