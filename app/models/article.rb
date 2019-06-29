b1e240b1ee93eb3bc85012e13f18375bcfacfa06
AJ Guiles <ashiguiles@gmail.com>
2019-06-29 03:49:50 +0000

    Done.

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,13 +2,7 @@
 
 class Article < ActiveRecord::Base
   
-  # attr_accessor :title, :article 
-  
-  # def initialize(title, article)
-  #   @title = title
-  #   @article = article
-  # end
-  
+
   
   
 end
